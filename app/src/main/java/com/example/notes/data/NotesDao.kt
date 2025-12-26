b3870ff39a1f1a0c61692df98018c0f1b0979098
Konstantin Sidnikhin <bentattooking@gmail.com>
2025-12-26 01:11:40 -0800

    3.27 NotesRepositoryImpl

--- app/src/main/java/com/example/notes/data/NotesDao.kt
+++ app/src/main/java/com/example/notes/data/NotesDao.kt
@@ -8,6 +8,9 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NotesDao {
+    @Query("SELECT * FROM notes WHERE id == :noteId")
+    suspend fun getNote(noteId: Int): NoteDbModel
+
     @Query("SELECT * FROM notes ORDER BY updatedAt DESC")
     suspend fun getNote(noteId: Int): NoteDbModel
     @Query("SELECT * FROM notes ORDER BY updatedAt DESC")
@@ -16,11 +19,11 @@ interface NotesDao {
     @Query("SELECT * FROM notes WHERE title LIKE '%' || :query || '%' OR content LIKE '%' || :query || '%' ORDER BY updatedAt DESC")
     fun searchNotes(query: String): Flow<List<NoteDbModel>>
 
-    @Query("DELETE FROM notes  WHERE id == :noteId" )
-    suspend fun deleteNote(noteId:Int)
+    @Query("DELETE FROM notes  WHERE id == :noteId")
+    suspend fun deleteNote(noteId: Int)
 
     @Query("UPDATE notes SET isPinned = NOT isPinned WHERE id == :noteId")
-    suspend fun switchPinnedStatus(noteId:Int)
+    suspend fun switchPinnedStatus(noteId: Int)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addNote(noteDbModel: NoteDbModel)
